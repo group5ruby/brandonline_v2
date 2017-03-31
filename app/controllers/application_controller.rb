f2f20154b1f6f980a6dc0d8cb55f802542148d94
luuphu25 <luuphu25@gmail.com>
2017-03-31 13:14:31 +0000

    verify help method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,18 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:index]
 
   helper_method :avatar
+  helper_method :is_verify
 
   def avatar(url)    
       url || "http://i.imgur.com/P2u37kw.png"
   end
   
+  def is_verify(value)
+      if value == true
+
+        return "checkmark icon verified"
+      else
+        return "help circle icon"
+      end
+  end
 end
