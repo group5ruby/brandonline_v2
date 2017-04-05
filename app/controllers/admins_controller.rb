930ee8b75a9696c4890f0d266f54281c3bec4f7d
luuphu25 <luuphu25@gmail.com>
2017-04-05 07:00:58 +0000

    fix eprofile

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -15,6 +15,14 @@ class AdminsController < ApplicationController
      @account_status = AccountStatus.find(@guest.account_status)  
      @feedbacks = Feedback.where(user_id: @guest.id).order("created_at desc")
       @icon = User.is_verified(@guest.identity_number) 
+    @account_color = "green" if @guest.account_status == 1
+    @account_color = "blue" if @guest.account_status == 2
+    @account_color = "yellow" if @guest.account_status == 3
+    @account_color = "orange" if @guest.account_status == 4
+    @account_color = "gray" if @guest.account_status == 5
+    @account_color = "red" if @guest.account_status == 6
+    @positive_feedbacks = Feedback.where(user_id: @guest.id, rating: '3').order("created_at desc")
+    @negative_feedbacks = Feedback.where(user_id: @guest.id, rating: '1').order("created_at desc")
   end
 
 end
